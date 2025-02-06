0867c8ee74784bae31c890859b939ddb43a0871c
Rachael <rachy.torok@gmail.com>
2025-02-06 16:03:09 -0500

    Task 1 - Customer Profile.

---
+++ cc_5.js
@@ -0,0 +1,4 @@
+// Task 1: Customer Profile
+const customer = { name: "John Doe", age: 35, email:"john.doe@email.com" };// add customer properties
+
+console.log(`Name: ${customer.name}, Age: ${customer.age}, Email: ${customer.email}`); // print customer properties 
