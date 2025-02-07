307b905b23fd2ceef02bd8fa400c70ca64892ec7
Rachael <rachy.torok@gmail.com>
2025-02-07 12:50:45 -0500

    Task 2 - Order Details.

--- cc_5.js
+++ cc_5.js
@@ -2,3 +2,11 @@
 const customer = { name: "John Doe", age: 35, email:"john.doe@email.com" };// add customer properties
 
 console.log(`Name: ${customer.name}, Age: ${customer.age}, Email: ${customer.email}`); // print customer properties 
+
+// Task 2: Order Details
+
+const order = {orderId: "12345", totalAmount: "150", status: "Processing"}// add order properties
+const displayOrder = (order) => {
+    console.log(`Order ID: ${order.orderId}, Total Amount: ${order.totalAmount}, Status: ${order.status}`);
+} // display order details using this function
+displayOrder(order); // call the function
